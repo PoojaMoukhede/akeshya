6cc3f5d667313181c6936dedad017a7e1c0b5051
Poojamoukhede <poojamoukhede27@gmail.com>
2024-06-10 15:29:57 +0530

    about us issue solved

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,13 +3,13 @@
 import Footer from "../components/Footer";
 import AboutUS from "../components/AboutUS";
 
-
 export default function about() {
   return (
     <>
-      <AboutUS />
-      <Footer/>
+      <div id="about_indi">
+        <AboutUS />
+      </div>
+      <Footer />
     </>
   );
 }
-   
